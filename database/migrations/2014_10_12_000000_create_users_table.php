67bef1126ae283399bd81425c257ffce521cb475
Botir <tiboruz@gmail.com>
2024-09-13 12:17:21 +0500

    3 commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('shop_id') ->nullable()->constrained();
+            $table->foreignId('role_id')->nullable()->constrained();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
